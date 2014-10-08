821bda63578eeecf2538dc459de229ad79fa515c
Jérémy Lal <kapouer@melix.org>
2014-10-08 11:56:08 +0200

    Always create web view with a content manager

--- src/webview.cc
+++ src/webview.cc
@@ -72,7 +72,7 @@ WebView::WebView(Handle<Object> opts) {
     }
   }
 
-  view = WEBKIT_WEB_VIEW(webkit_web_view_new());
+  view = WEBKIT_WEB_VIEW(webkit_web_view_new_with_user_content_manager(webkit_user_content_manager_new()));
 
   window = gtk_offscreen_window_new();
   gtk_container_add(GTK_CONTAINER(window), GTK_WIDGET(view));
