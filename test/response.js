deacd96feb7286b8500abc155340252694640172
Jérémy Lal <kapouer@melix.org>
2014-10-16 10:23:46 +0200

    Add tests for response properties

--- test/response.js
+++ test/response.js
@@ -15,6 +15,26 @@ describe("response handler data method", function suite() {
 			}
 		});
 	});
+	it("should get headers from the response", function(done) {
+		this.timeout(5000);
+		WebKit().load("https://www.debian.org/logos/openlogo-nd-100.png").on("response", function(response) {
+			if (response.uri == this.uri) {
+				expect(response.headers['Content-Type']).to.be('image/png');
+				expect(Object.keys(response.headers).length).to.be.greaterThan(3);
+				done();
+			}
+		});
+	});
+	it("should get length status mime and filename from the response", function(done) {
+		this.timeout(5000);
+		WebKit().load("https://www.debian.org/logos/openlogo-nd-100.png").on("response", function(response) {
+			if (response.uri == this.uri) {
+				expect(response.mime).to.be('image/png');
+				expect(response.status).to.be(200);
+				expect(response.filename).to.be(null);
+				expect(response.length).to.be.greaterThan(1000);
+				done();
+			}
+		});
+	});
 });
-
-
