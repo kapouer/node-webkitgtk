c1c1ef19978a644432ffc7f682caba17b0675fc1
Jérémy Lal <kapouer@melix.org>
2014-11-01 11:17:02 +0100

    Make cookies tests not pollute one each other

--- test/cookie.js
+++ test/cookie.js
@@ -8,13 +8,13 @@ describe("cookies option", function suite() {
 		var count = 0;
 		var cookiestr = "mycookie=myvalue";
 		var server = require('http').createServer(function(req, res) {
-			if (req.url == "/") count++;
+			if (req.url == "/test/") count++;
 			if (count == 2) expect(req.headers.cookie).to.be(cookiestr);
 			res.write('<html><body><img src="myimg.png"/></body></html>');
 			res.end();
 		}).listen(8008);
 
-		WebKit().load("http://localhost:8008", {cookies:cookiestr + ";Path=/"}, function(err, view) {
+		WebKit().load("http://localhost:8008/test/", {cookies:cookiestr + ";Path=/test"}, function(err, view) {
 			expect(err).to.not.be.ok();
 			expect(count).to.be(2);
 			setImmediate(function() {
@@ -28,17 +28,17 @@ describe("cookies option", function suite() {
 		var cookiestr = "mycookie=myvalue";
 		var cookiestr2 = "mycookie=myvalue2";
 		var server = require('http').createServer(function(req, res) {
-			if (req.url == "/") count++;
+			if (req.url == '/test2/') count++;
 			if (count == 4) expect(req.headers.cookie).to.be(cookiestr2);
 			res.write('<html><body><img src="myimg.png"/></body></html>');
 			res.end();
 		}).listen(8009);
 
-		WebKit().load("http://localhost:8009", {cookies:cookiestr + ";Path=/"}, function(err, view) {
+		WebKit().load("http://localhost:8009/test2/", {cookies:cookiestr + ";Path=/test2"}, function(err, view) {
 			expect(err).to.not.be.ok();
 			expect(count).to.be(2);
 			view.unload(function() {
-				view.load("http://localhost:8009", {cookies:cookiestr2 + ";Path=/"}, function(err) {
+				view.load("http://localhost:8009/test2/", {cookies:cookiestr2 + ";Path=/test2"}, function(err) {
 					expect(err).to.not.be.ok();
 					expect(count).to.be(4);
 					setImmediate(function() {
