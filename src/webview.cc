7c71d8b3066dd6e71a1e4c457f37910615493d94
Jérémy Lal <kapouer@melix.org>
2015-02-17 10:26:07 +0100

    Uncomment to debug

--- src/webview.cc
+++ src/webview.cc
@@ -330,6 +330,7 @@ void WebView::updateUri(const gchar* uri) {
 void WebView::Change(WebKitWebView* web_view, WebKitLoadEvent load_event, gpointer data) {
 	WebView* self = (WebView*)data;
 	const gchar* uri = webkit_web_view_get_uri(web_view);
+//	g_print("change %d %d %s %s\n", load_event, self->state, self->uri, uri);
 	switch (load_event) {
 		case WEBKIT_LOAD_STARTED: // 0
 			/* New load, we have now a provisional URI */
@@ -374,7 +375,7 @@ void WebView::Change(WebKitWebView* web_view, WebKitLoadEvent load_event, gpoint
 
 gboolean WebView::Fail(WebKitWebView* web_view, WebKitLoadEvent load_event, gchar* failing_uri, GError* error, gpointer data) {
 	WebView* self = (WebView*)data;
-	// g_print("fail %d %d %s %s\n", load_event, self->state, self->uri, failing_uri);
+//  g_print("fail %d %d %s %s\n", load_event, self->state, self->uri, failing_uri);
 	if (self->nextUri == NULL && self->state == DOCUMENT_LOADING && g_strcmp0(failing_uri, self->uri) == 0) {
 		if (self->loadCallback != NULL) {
 			self->state = DOCUMENT_ERROR;
