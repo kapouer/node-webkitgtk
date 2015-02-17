d876be3906a8ecdd03a3cf6c4bb5125518025f35
Jérémy Lal <kapouer@melix.org>
2015-02-17 10:41:52 +0100

    Add a test for xhr immediate cancel

--- test/request.js
+++ test/request.js
@@ -91,4 +91,44 @@ describe("request listener", function suite() {
 			}, 100);
 		});
 	});
+
+	it("should cancel xhr requests immediately", function(done) {
+		// this typically is not the case if ret = TRUE in src/webextension.cc
+		// and works if uri is set to ""
+		this.timeout(1000);
+		var doc = '<html><head>\
+		<script type="text/javascript">var xhr = new XMLHttpRequest();\
+			xhr.open("GET", "/test", true);\
+			xhr.setRequestHeader("Content-Type", "application/json; charset=utf-8");\
+			xhr.send();</script></head>\
+		<body>move along</body></html>'
+		var server = require('http').createServer(function(req, res) {
+			if (req.url == "/") {
+				res.statusCode = 200;
+				res.end(doc);
+			} else if (req.url == "/test") {
+				res.statusCode = 200;
+				setTimeout(function() {
+					res.end('{"hello": "tata"}');
+				}, 3000);
+			} else {
+				expect("no 404").to.be("should happen");
+				res.statusCode = 404;
+				res.end();
+			}
+		}).listen(8025);
+		WebKit().load("http://localhost:8025", {console:true, stall: 2000})
+		.on('request', function(req) {
+			if (req.uri.indexOf('test') > 0) {
+				req.cancel = true;
+			}
+		})
+		.wait('idle').html(function(err, str) {
+			expect(str).to.be(doc);
+			setTimeout(function() {
+				server.close();
+				done();
+			}, 100);
+		});
+	});
 });
