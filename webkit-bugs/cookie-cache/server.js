d6bd6f4f7c4dd08b49f07cba78309983f7a2c778
Jérémy Lal <kapouer@melix.org>
2015-07-14 15:52:14 +0200

    Track important upstream webkit bugs here

---
+++ webkit-bugs/cookie-cache/server.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+var server = require('http').createServer(function(req, res) {
+	res.setHeader('Vary', 'Cookie');
+	if (req.url == "/xhr") {
+		console.log("received xhr request with headers", req.headers);
+		if (req.headers.cookie == 'sid=secondcookie') {
+			console.error("*** received secondcookie instead of firstcookie ***");
+		}
+		res.setHeader("Content-Type", "application/json");
+		res.write('{"some": "json"}');
+		res.end();
+	} else {
+		console.error("should not receive", req.url);
+		res.writeHead(404);
+		res.end();
+	}
+}).listen(40001);
