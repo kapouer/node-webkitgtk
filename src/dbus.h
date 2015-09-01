298cbff40987deec3c17a49c99e2a10e64c6757e
Jérémy Lal <kapouer@melix.org>
2015-09-01 10:05:24 +0200

    Remove old file

--- src/dbus.h
+++
@@ -1,8 +0,0 @@
-#ifndef WEBKITGTK_DBUS_H
-#define WEBKITGTK_DBUS_H
-
-
-#define DBUS_OBJECT_WKGTK "/org/nodejs/WebKitGtk/WebView"
-#define DBUS_INTERFACE_WKGTK "org.nodejs.WebKitGtk.WebView"
-
-#endif
