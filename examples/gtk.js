673ca37052fac44e145ec79e8bbf692f44e67fa4
Jérémy Lal <kapouer@melix.org>
2015-09-21 18:13:07 +0200

    Update gtk example

--- examples/gtk.js
+++ examples/gtk.js
@@ -1,15 +1,17 @@
 var W = require('../')
-W({offscreen:false})
-.load('http://granze.github.io/flip-clock/components/flip-clock/demo.html', {
+W.load('http://granze.github.io/flip-clock/components/flip-clock/demo/', {
+	offscreen: false,
 	transparent: true,
 	decorated: false,
 	style: 'html,body {background-color:transparent !important;opacity:0.8}',
 	width: 600,
 	height: 200
 })
-.wait('load').run(function() {
-	var f = document.querySelector('flip-clock');
-	document.body.innerHTML = "";
-	document.body.appendChild(f);
-})
-.wait('unload')
+.once('ready', function() {
+	this.run(function() {
+		var f = document.querySelector('flip-clock');
+		document.body.innerHTML = "";
+		document.body.appendChild(f);
+	});
+});
+
