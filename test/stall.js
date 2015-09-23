c8075cb19daf66ca9a83365b8c8076a60c1d7b68
Jérémy Lal <kapouer@melix.org>
2015-09-23 12:48:55 +0200

    Add test for new options

---
+++ test/stall.js
@@ -0,0 +1,82 @@
+var WebKit = require('../');
+var expect = require('expect.js');
+var fs = require('fs');
+var join = require('path').join;
+
+
+describe("stall load options", function suite() {
+	it("should not account for idling if > stallTimeout", function(done) {
+		var port;
+		var server = require('http').createServer(function(req, res) {
+			res.statusCode = 200;
+			res.end('<html><head><script type="text/javascript">\
+			setTimeout(function() {window.testDone = true;}, 400);\
+			</script></head><body>test</body></html>');
+		}).listen(function() {
+			port = server.address().port;
+			WebKit(function(err, w) {
+				// increasing stall here to 1000 will fail the test (expectedly)
+				w.load("http://localhost:" + port, {stallTimeout:100, console: true})
+				.once('idle', function() {
+					this.run(function(cb) {
+						cb(null, window.testDone ? 'yes' : 'no');
+					}, function(err, result) {
+						expect(result).to.be('no');
+						server.close();
+						done();
+					});
+				});
+			});
+		});
+	});
+	it("should account for idling if < stallTimeout", function(done) {
+		var port;
+		var server = require('http').createServer(function(req, res) {
+			res.statusCode = 200;
+			res.end('<html><head><script type="text/javascript">\
+			setTimeout(function() {window.testDone = true;}, 400);\
+			</script></head><body>test</body></html>');
+		}).listen(function() {
+			port = server.address().port;
+			WebKit(function(err, w) {
+				// increasing stall here to 1000 will fail the test (expectedly)
+				w.load("http://localhost:" + port, {stallTimeout:500, console: true})
+				.once('idle', function() {
+					this.run(function(cb) {
+						cb(null, window.testDone ? 'yes' : 'no');
+					}, function(err, result) {
+						expect(result).to.be('yes');
+						server.close();
+						done();
+					});
+				});
+			});
+		});
+	});
+	it("should not account for idling if > stallInterval", function(done) {
+		var port;
+		var server = require('http').createServer(function(req, res) {
+			res.statusCode = 200;
+			res.end('<html><head><script type="text/javascript">\
+			window.testDone = 0;\
+			setInterval(function() {window.testDone++;}, 40);\
+			</script></head><body>test</body></html>');
+		}).listen(function() {
+			port = server.address().port;
+			WebKit(function(err, w) {
+				// increasing stall here to 1000 will fail the test (expectedly)
+				w.load("http://localhost:" + port, {stallInterval:100, console: true})
+				.once('idle', function() {
+					this.run(function(cb) {
+						cb(null, window.testDone);
+					}, function(err, result) {
+						expect(result).to.be(2);
+						server.close();
+						done();
+					});
+				});
+			});
+		});
+	});
+});
+
