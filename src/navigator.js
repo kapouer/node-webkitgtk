3ee62650657ad6d626f0523da014a9796a008573
Jérémy Lal <kapouer@melix.org>
2016-03-23 14:28:16 +0100

    Safer navigator output

--- src/navigator.js
+++ src/navigator.js
@@ -1,8 +1,24 @@
 var W = require('..');
 
 W.load("", function(err, w) {
-	w.run('navigator', function(err, ua) {
-		console.log(JSON.stringify(ua));
+	w.run(function() {
+		function ser(s, d) {
+			for (var k in d) {
+				var val = d[k];
+				if (typeof val == "function") continue;
+				else if (typeof val == "object") {
+					s[k] = {};
+					ser(s[k], val);
+				} else {
+					s[k] = val;
+				}
+			}
+		}
+		var obj = {};
+		ser(obj, navigator);
+		return obj;
+	}, function(err, ua) {
+		console.log(JSON.stringify(ua, null, "  "));
 		w.destroy(function(err) {
 			process.exit();
 		});
