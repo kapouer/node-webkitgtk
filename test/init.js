8cf4bd7969098b7678ed13114e2ce7de4e5bfddd
Jérémy Lal <kapouer@melix.org>
2016-07-27 16:39:43 +0200

    Test on-disk clearing of cache

--- test/init.js
+++ test/init.js
@@ -20,23 +20,24 @@ describe("init method", function suite() {
 		});
 	});
 	it("should clear cache", function(done) {
-		this.timeout(10000);
 		var called = false;
 		var port;
 		var count = 0;
 		var server = require('http').createServer(function(req, res) {
-			if (req.path != '/') {
+			if (req.url != '/') {
 				res.statusCode = 404;
 				res.end("Not Found");
 			} else {
 				res.statusCode = 200;
-				res.setHeader('Cache-Control: max-age=100');
+				res.setHeader('Cache-Control', 'public, max-age=100');
 				count++;
 				res.end("stored text");
 			}
 		}).listen(function() {
 			port = server.address().port;
-			WebKit({cacheDir: "cache/test2"}).then(function(w) {
+			var w;
+			WebKit({cacheDir: "cache/test2"}).then(function(inst) {
+				w = inst;
 				return w.load("http://localhost:" + port);
 			}).then(function() {
 				return w.load("http://localhost:" + port);
@@ -46,6 +47,55 @@ describe("init method", function suite() {
 				return w.load("http://localhost:" + port);
 			}).then(function() {
 				expect(count).to.be(2);
+				done();
+			}).catch(function(err) {
+				setImmediate(function() {
+					throw err;
+				});
+			});
+		});
+	});
+	it("should clear cache for next instance", function(done) {
+		var called = false;
+		this.timeout(6000);
+		var port;
+		var count = 0;
+		var bigBuf = Buffer(2000000).fill("h");
+		var server = require('http').createServer(function(req, res) {
+			if (req.url != '/') {
+				res.statusCode = 404;
+				res.end("Not Found");
+			} else {
+				res.statusCode = 200;
+				res.setHeader('Cache-Control', 'public, max-age=1000');
+				count++;
+				res.end(bigBuf);
+			}
+		}).listen(function() {
+			port = server.address().port;
+			var w;
+			WebKit({cacheDir: "cache/test4"}).then(function(inst) {
+				w = inst;
+				return w.load("http://localhost:" + port);
+			}).then(function() {
+				return w.unload().then(function() {
+					// clear cache after unloading or else resources are not freed
+					w.clearCache();
+					return w.destroy();
+				});
+			}).then(function() {
+				w = null;
+				return WebKit({cacheDir: "cache/test4"}).then(function(inst) {
+					w = inst;
+					return w.load("http://localhost:" + port);
+				});
+			}).then(function() {
+				expect(count).to.be(2);
+				done();
+			}).catch(function(err) {
+				setImmediate(function() {
+					throw err;
+				});
 			});
 		});
 	});
