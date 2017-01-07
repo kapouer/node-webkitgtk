2e8a4089b415ec8afc3eeb37983282f5f5860bcc
Jérémy Lal <kapouer@melix.org>
2017-01-07 02:33:05 +0100

    Hook all console methods

--- src/client-console.js
+++ src/client-console.js
@@ -1,15 +1,15 @@
 module.exports = function consoleEmitter(emit) {
 	if (!window.console) return;
+	Object.keys(console).forEach(function(meth) {
+		window.console[meth] = function() {
+			var args = ['console', meth].concat(Array.from(arguments));
+			emit.apply(null, args);
+		};
+	});
 	window.console.trace = function() {
 		var args = Array.from(arguments);
 		args.push(new Error());
 		args = ['console', 'trace'].concat(args);
 		emit.apply(null, args);
 	};
-	['log', 'error', 'info', 'warn'].forEach(function(meth) {
-		window.console[meth] = function() {
-			var args = ['console', meth].concat(Array.from(arguments));
-			emit.apply(null, args);
-		};
-	});
 };
