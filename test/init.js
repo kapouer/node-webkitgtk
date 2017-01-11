73f86d6d6677914a388d32b8d386209c23b9feb3
Jérémy Lal <kapouer@melix.org>
2017-01-11 21:59:32 +0100

    Improve cache test

--- test/init.js
+++ test/init.js
@@ -1,10 +1,16 @@
 var WebKit = require('../');
 var expect = require('expect.js');
 var fs = require('fs');
+var Path = require('path');
+var glob = require('glob');
+var rimraf = require('rimraf');
 
 // this test doesn't really work
 
 describe("init method", function suite() {
+	before(function(done) {
+		rimraf(Path.join(__dirname, '..', 'cache/'), done);
+	});
 	it("should initialize cacheDir with call to init", function(done) {
 		this.timeout(10000);
 		var called = false;
@@ -57,10 +63,11 @@ describe("init method", function suite() {
 	});
 	it("should clear cache for next instance", function(done) {
 		var called = false;
-		this.timeout(6000);
+		this.timeout(15000);
 		var port;
 		var count = 0;
-		var bigBuf = Buffer(2000000).fill("h");
+		var bufSize = 1600000;
+		var bigBuf = Buffer(bufSize).fill("h");
 		var server = require('http').createServer(function(req, res) {
 			if (req.url != '/') {
 				res.statusCode = 404;
@@ -68,6 +75,7 @@ describe("init method", function suite() {
 			} else {
 				res.statusCode = 200;
 				res.setHeader('Cache-Control', 'public, max-age=1000');
+				res.setHeader('Content-Type', 'text/plain');
 				count++;
 				res.end(bigBuf);
 			}
@@ -79,9 +87,21 @@ describe("init method", function suite() {
 				return w.load("http://localhost:" + port);
 			}).then(function() {
 				return w.unload().then(function() {
-					// clear cache after unloading or else resources are not freed
-					w.clearCache();
-					return w.destroy();
+					return new Promise(function(resolve, reject) {
+						glob("cache/test4/**/*", {nodir: true}, function(err, list) {
+							if (err) return reject(err);
+							if (!list.length) return reject(new Error("no blob in cache"));
+							fs.stat(list[0], function(err, stat) {
+								if (err) return reject(err);
+								expect(stat.size).to.be(bufSize);
+								resolve();
+							});
+						});
+					}).then(function() {
+						// clear cache after unloading or else resources are not freed
+						w.clearCache();
+						return w.destroy();
+					})
 				});
 			}).then(function() {
 				w = null;
